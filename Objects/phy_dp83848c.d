f672d779e11f270ceb4cb08893371e17c37b23a3
bigmammoth <justdoublecats@gmail.com>
2022-04-07 21:12:41 +0800

    Fixed compiling issues for the first time.

---
+++ Objects/phy_dp83848c.d
@@ -0,0 +1,9 @@
+.\objects\phy_dp83848c.o: D:\Keil_v5\Arm\Packs\ARM\CMSIS-Driver\2.7.0\ETH\PHY_DP83848C.c
+.\objects\phy_dp83848c.o: .\RTE\_Target_STM32F407VET6\Pre_Include_Global.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\Arm\Packs\ARM\CMSIS-Driver\2.7.0\ETH\PHY_DP83848C.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_ETH_PHY.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_ETH.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_Common.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\phy_dp83848c.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
